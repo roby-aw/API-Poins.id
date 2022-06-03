932cd702c6b7b6508c5f2d4f4b974a091fbc6b2f
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-06-03 16:16:54 +0700

    Fix : swagger

--- docs/docs.go
+++ docs/docs.go
@@ -783,7 +783,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "api-dummy.herokuapp.com",
+	Host:             "api-poins-id.herokuapp.com",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "API Jasa Pengiriman",
