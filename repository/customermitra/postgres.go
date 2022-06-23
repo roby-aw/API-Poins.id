4462d2337078748042ae35f4fef88e670fdb8c35
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-06-23 14:34:55 +0700

    fix update poin

--- repository/customermitra/postgres.go
+++ repository/customermitra/postgres.go
@@ -156,7 +156,8 @@ func (repo *PosgresRepository) ClaimPulsa(Data *customermitra.RedeemPulsaData) e
 	}
 	fmt.Println(Customers)
 	hasil := Customers.Poin - Data.Poin_redeem
-	err = repo.db.Model(&customermitra.Customers{}).Where("ID = ?", Data.Customer_id).Updates(&customermitra.Customers{Poin: hasil}).Error
+	Customers.Poin = hasil
+	err = repo.db.Model(Customers).Select("Poin").Updates(Customers).Error
 	if err != nil {
 		return err
 	}
