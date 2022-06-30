64ffbe6561609f972cdeb1e911a3954a80e5485e
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-06-30 14:28:41 +0700

    fix : update store

--- repository/admin/postgres.go
+++ repository/admin/postgres.go
@@ -354,7 +354,9 @@ func (repo *PosgresRepository) UpdateStore(store admin.UpdateStore) (*admin.Upda
 				return
 			}
 			err = errors.New("Email already used")
+			fmt.Println(err.Error())
 		}()
+		fmt.Println(err.Error())
 		if err != nil {
 			return nil, err
 		}
