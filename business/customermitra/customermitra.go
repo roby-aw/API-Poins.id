58a299be575a90336b91414edf81cefe958b6b33
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-07-06 00:07:54 +0700

    fix register customer

--- business/customermitra/customermitra.go
+++ business/customermitra/customermitra.go
@@ -100,7 +100,7 @@ type RegisterCustomer struct {
 	Fullname string `json:"fullname" validate:"required"`
 	Password string `json:"password" validate:"required"`
 	No_hp    string `json:"no_hp" validate:"required,numeric"`
-	Pin      int    `json:"pin" validate:"required,numeric"`
+	Pin      int    `json:"pin" validate:"required,numeric,lt=5"`
 }
 
 type Login struct {
