bba9ad348dbdf78165fddc243380478e2939ccb0
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-07-05 19:23:51 +0700

    fix model redeem

--- business/customermitra/customermitra.go
+++ business/customermitra/customermitra.go
@@ -100,7 +100,7 @@ type RegisterCustomer struct {
 	Fullname string `json:"fullname" validate:"required"`
 	Password string `json:"password" validate:"required"`
 	No_hp    string `json:"no_hp" validate:"required,numeric"`
-	Pin      int    `json:"pin" validate:"required"`
+	Pin      int    `json:"pin" validate:"required,lt=5,numeric"`
 }
 
 type Login struct {
