0c14bdcce3f2bbda747a49e9fe917e0495e21605
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-07-12 17:42:41 +0700

    fix cors

--- main.go
+++ main.go
@@ -46,10 +46,7 @@ func main() {
 	e := echo.New()
 	handleSwagger := echoSwagger.WrapHandler
 	e.GET("/swagger/*", handleSwagger)
-	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins: []string{"*"},
-		AllowHeaders: []string{echo.HeaderAuthorization, echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAccessControlAllowOrigin},
-	}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{}))
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "time=${time_rfc3339_nano}, method=${method}, uri=${uri}, status=${status}\n",
 	}))
