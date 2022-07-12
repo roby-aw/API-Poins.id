c26b21a574960369e1cf0d1ebac1beb31001f2c9
Roby Awaluddin <61074887+roby-aw@users.noreply.github.com>
2022-07-12 21:01:59 +0700

    fix code create customer

--- repository/customer/postgres.go
+++ repository/customer/postgres.go
@@ -401,7 +401,7 @@ func (repo *PosgresRepository) InsertStore(store *customerBusiness.RegisterStore
 		err := errors.New("Email already use")
 		return nil, err
 	}
-	err := repo.db.Create(&customerBusiness.Store{Email: store.Email, Password: string(hash), Store: store.Store, Alamat: store.Alamat}).Error
+	err := repo.db.Model(&repository.Customer{}).Create(&customerBusiness.Store{Email: store.Email, Password: string(hash), Store: store.Store, Alamat: store.Alamat}).Error
 	if err != nil {
 		return nil, err
 	}
